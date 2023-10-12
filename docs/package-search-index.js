63bb71197c91769514533eaf8fe7dc647c821e19
foxobyte <foxobytestream@gmail.com>
2023-10-12 02:16:29 -0400

    Initial Growth Rates Equations Javadocs

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.foxobyte.pokemon.constant"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.foxobyte.pokemon.calculator"},{"l":"edu.foxobyte.pokemon.constant"}]
